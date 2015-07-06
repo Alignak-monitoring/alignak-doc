de8b46c54b5643be5fb8bd2de2ff3133b1228014
Sebastien Coavoux <sebastien.coavoux@savoirfairelinux.com>
2015-07-06 09:54:28 -0400

    Enh: Make it work on rtd

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,9 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
+#sys.path.insert(0, os.path.abspath('../..'))
 import alignak
-from alignak.bin import VERSION
+from alignak.version import VERSION
 
 # Fix for missing modules
 class Mock(object):
@@ -48,7 +48,7 @@ MOCK_MODULES = ['MySQLdb',
                 'cx_Oracle',
                 'log',
                 'pymongo',
-                #'pycurl',
+                'requests',
                 ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()

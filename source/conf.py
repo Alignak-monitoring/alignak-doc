d13a66def102d0415ebacc7bf96322b085ceb4ab
David Durieux <d.durieux@siprossii.com>
2015-10-06 14:54:15 +0200

    Try fic ocnfig.py

--- source/conf.py
+++ source/conf.py
@@ -18,8 +18,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('../..'))
-import alignak
-from alignak.version import VERSION
+#import alignak
+#from alignak.version import VERSION
 
 # Fix for missing modules
 class Mock(object):
